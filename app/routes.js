2e50da83b9e0c47c18d3ca3853a917c2530ecc69
The-Dagda <99957636+The-Dagda@users.noreply.github.com>
2022-02-18 09:40:22 +0000

    Initial commit

---
+++ app/routes.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const router = express.Router()
+
+// Add your routes here - above the module.exports line
+router.post('/elig1-answer', function (req, res) {
+  var elig1answer = req.session.data['current-former']
+  if (elig1answer == "other"){
+      res.redirect('/Unelig1')
+  } else {
+        res.redirect('/Elig2')
+  }
+})
+
+router.post('/elig2-answer', function (req, res) {
+  var elig2answer = req.session.data['Manager-ask']
+  if (elig2answer == "No"){
+      res.redirect('/Unelig2')
+  } else {
+        res.redirect('/Elig3')
+  }
+})
+
+router.post('/elig3-answer', function (req, res) {
+  var elig3answer = req.session.data['current-last-grade']
+  if (elig3answer == "None of the above"){
+      res.redirect('/Unelig3')
+  } else {
+        res.redirect('/Elig4')
+  }
+})
+
+router.post('/elig4-answer', function (req, res) {
+  var elig4answer = req.session.data['current-last-role']
+  if (elig4answer == "None of the above"){
+      res.redirect('/Unelig4')
+  } else {
+        res.redirect('/proceed1')
+  }
+})
+
+router.post('/proan-answer', function (req, res) {
+  var proans = req.session.data['pronans1']
+  if (proans == "Continue"){
+      res.redirect('/who')
+  } else {
+        res.redirect('/start')
+  }
+})
+
+router.post('/typeofrole-answer', function (req, res) {
+  var typeofroleans = req.session.data['current-former-role']
+  if (typeofroleans == "Yes"){
+      res.redirect('/recentcivil')
+  } else {
+        res.redirect('/notrecentcivil')
+  }
+})
+
+module.exports = router
